95bf9d29e688d48f52413d8b6600f25d7d7b0872
Aleksey Gavrilov <a.gavrilov@semrush.com>
2021-03-22 14:30:20 +0100

    Do not require arrow keys modifier for SHIFT+GUI
    Add WINS layer

--- keyboards/gboards/k/gergoplex/keymaps/prog13/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/prog13/keymap.c
@@ -2,8 +2,9 @@
 #include "g/keymap_combo.h"
 
 #define BASE 0
-#define SYMB 1
-#define NUMB 2
+#define WINS 1
+#define SYMB 2
+#define NUMB 3
 
 /*
  * ┌──────┬──────┬──────┬──────┬──────┐                     ┌──────┬──────┬──────┬──────┬──────┐
@@ -13,9 +14,9 @@
  * ├──────┼──────┼──────┼──RMB─┼──LMB─┤                     ├──────┼──────┼──────┼──────┼──────┤
  * │      │     MINS   ENT     │      │                     │     QUO    UNDR    │      │      │
  * └──────┴──────┴──────┴──────┴──────┘                     └──────┴──────┴──────┴──────┴──────┘
- *               ┌──────┬──────┬──────┐                     ┌──────┬──────┬──────┐
- *               │      │      │      │                     │      │      │      │
- *               └──────┴──────┴──────┘                     └──────┴──────┴──────┘
+ *         ┌────────┬────────┬────────┐                     ┌────────┬────────┬────────┐
+ *         │        │        │        │                     │        │        │        │
+ *         └────────┴────────┴────────┘                     └────────┴────────┴────────┘
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -40,19 +41,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /*
+     * ┌──────┬──────┬──────┬──────┬──────┐                 ┌──────┬──────┬──────┬──────┬──────┐
+     * │   Q  │   W  │   E  │   R  │   T  │                 │   Y  │   U  │   I  │   O  │   P  │
+     * ├──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┤
+     * │LGUI/A│   S  │   D  │   F  │   G  │                 │   H  │   J  │   K  │   L  │CTRL/;|
+     * ├──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┤
+     * │SHFT/Z│   X  │   C  │   V  │   B  │                 │   N  │   M  │   ,  │   .  │SHFT/?│
+     * └──────┴──────┴──────┴──────┴──────┘                 └──────┴──────┴──────┴──────┴──────┘
+     *         ┌────────┬────────┬────────┐                 ┌────────┬────────┬────────┐
+     *         │  RALT  │  CTRL  │SPC(SYM)│                 │ENT(NUM)│  BSPC  │SHFT/TAB│
+     *         └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+     */
+    [WINS] = LAYOUT_gergoplex(
+        KC_Q,               KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I,    KC_O,   KC_P,
+        MT(MOD_LGUI, KC_A), KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K,    KC_L,   MT(MOD_RCTL, KC_SCLN),
+        MT(MOD_LSFT, KC_Z), KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
+
+        KC_RALT, KC_LCTL, LT(SYMB, KC_SPC),                 LT(NUMB, KC_ENT), KC_BSPC, MT(MOD_RSFT, KC_TAB)
+    ),
+
+    /*
     * ┌──────┬──────┬──────┬──────┬──────┐                  ┌──────┬──────┬──────┬──────┬──────┐
-    * │   !  │   @  │   {  │   }  │   |  │                  │   `  │   ~  │   \  │      │      │
+    * │   !  │   @  │   {  │   }  │   |  │                  │   `  │   ~  │   \  │      │ WINS │
     * ├──────┼──────┼──────┼──────┼──────┤                  ├──────┼──────┼──────┼──────┼──────┤
     * │   #  │   $  │   (  │   )  │  LMB │                  │   +  │   -  │   /  │   *  │   '  |
     * ├──────┼──────┼──────┼──────┼──────┤                  ├──────┼──────┼──────┼──────┼──────┤
     * │   %  │   ^  │   [  │   ]  │  RMB │                  │   &  │   =  │   ,  │   .  │SHFT/?│
     * └──────┴──────┴──────┴──────┴──────┘                  └──────┴──────┴──────┴──────┴──────┘
     *         ┌────────┬────────┬────────┐                  ┌────────┬────────┬────────┐
-    *         │  RALT  │  LGUI  │SPC(SYM)│                  │ SCOLON │  BSPC  │SHFT/TAB│
+    *         │  RALT  │  LGUI  │        │                  │ SCOLON │  BSPC  │SHFT/TAB│
     *         └────────┴────────┴────────┘                  └────────┴────────┴────────┘
     */
     [SYMB] = LAYOUT_gergoplex(
-        KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,        KC_GRV,  KC_TILD, KC_BSLS, KC_NO,   KC_NO,
+        KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,        KC_GRV,  KC_TILD, KC_BSLS, KC_NO,   TG(WINS),
         KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,        KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
         KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,        KC_AMPR, KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS,
 
@@ -68,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * │  F6  │  F7  │  F8  │  F9  │  F10 │                  │M_LEFT│M_DOWN│ M_UP │M_RGHT│VOL_DN│
     * └──────┴──────┴──────┴──────┴──────┘                  └──────┴──────┴──────┴──────┴──────┘
     *         ┌────────┬────────┬────────┐                  ┌────────┬────────┬────────┐
-    *         │   F11  │   F12  │    =   │                  │ENT(NUM)│  PLAY  │  SKIP  │
+    *         │   F11  │   F12  │SPC(SYM)│                  │        │  PLAY  │  SKIP  │
     *         └────────┴────────┴────────┘                  └────────┴────────┴────────┘
     */
     [NUMB] = LAYOUT_gergoplex(
@@ -76,6 +97,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5,                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
         KC_F6,  KC_F7,  KC_F8, KC_F9, KC_F10,               KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
 
-        KC_F11, KC_F12, KC_EQL,                             KC_TRNS, KC_MPLY, KC_MNXT
-    )
+        KC_F11, KC_F12, KC_TRNS,                            KC_TRNS, KC_MPLY, KC_MNXT
+    ),
+};
+
+uint8_t mod_state;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  mod_state = get_mods();
+
+  switch (keycode) {
+    case KC_H:
+      if (mod_state & MOD_MASK_SG) {
+        if (record->event.pressed) {
+          // Key pressed
+          register_code(KC_LEFT);
+        } else {
+          // Key released
+          unregister_code(KC_LEFT);
+        }
+      }
+      return false; // Skip all further processing of this key
+
+    case KC_J:
+      if (mod_state & MOD_MASK_SG) {
+        if (record->event.pressed) {
+          register_code(KC_DOWN);
+        } else {
+          unregister_code(KC_DOWN);
+        }
+      }
+      return false;
+
+    case KC_K:
+      if (mod_state & MOD_MASK_SG) {
+        if (record->event.pressed) {
+          register_code(KC_UP);
+        } else {
+          unregister_code(KC_UP);
+        }
+      }
+      return false;
+
+    case KC_L:
+      if (mod_state & MOD_MASK_SG) {
+        if (record->event.pressed) {
+          register_code(KC_RIGHT);
+        } else {
+          unregister_code(KC_RIGHT);
+        }
+      }
+      return false;
+
+    default:
+      return true; // Process all other keycodes normally
+  }
 };
