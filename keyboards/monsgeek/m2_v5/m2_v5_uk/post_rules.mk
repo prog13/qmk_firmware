50ceecfb941d0db742cdaf0cd4b8f62e2c27beee
sdk <2371260634@qq.com>
2025-04-21 16:37:12 +0800

    add m2 v5 keyboard

---
+++ keyboards/monsgeek/m2_v5/m2_v5_uk/post_rules.mk
@@ -0,0 +1,6 @@
+RGB_MATRIX_CUSTOM_USER = yes
+RGBLIGHT_DRIVER = custom
+include keyboards/monsgeek/m2_v5/m2_v5_uk/rgb_record/rgb_record.mk
+include keyboards/monsgeek/m2_v5/m2_v5_uk/wls/wls.mk
+include keyboards/linker/wireless/wireless.mk
+
---
+++ keyboards/monsgeek/m2_v5/m2_v5_us/post_rules.mk
@@ -0,0 +1,6 @@
+RGB_MATRIX_CUSTOM_USER = yes
+RGBLIGHT_DRIVER = custom
+include keyboards/monsgeek/m2_v5/m2_v5_us/rgb_record/rgb_record.mk
+include keyboards/monsgeek/m2_v5/m2_v5_us/wls/wls.mk
+include keyboards/linker/wireless/wireless.mk
+
