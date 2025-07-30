4f7c7ff981857aaecc32904dddd90374ceda4a1b
yangzheng <1345509173@q.com>
2025-07-30 16:05:55 +0800

    delete mineral_01_us mineral_02_us keyboard

--- keyboards/monsgeek/mineral_02/post_rules.mk
+++
@@ -1,8 +0,0 @@
-SRC += rgb_light_mode.c
-RGB_MATRIX_CUSTOM_USER = yes
-RGBLIGHT_DRIVER = custom
-
-include keyboards/monsgeek/mineral_02/rgb_record/rgb_record.mk
-include keyboards/monsgeek/mineral_02/wls/wls.mk
-include keyboards/linker/wireless/wireless.mk
-
